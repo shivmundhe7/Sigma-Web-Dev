8a7097bbe8fddc7df36d0c4e1a463c1bb5a7a0cc
shivmundhe7 <shivmundhe16@gmail.com>
2025-07-05 00:02:53 +0530

    web dev

--- Variables/script.js
+++ Variables/script.js
@@ -1,8 +1,8 @@
-console.log("Hello world")
+console.log("Hello world");
 
 var a = 5;
 var b = 6;
 var c = "harry";
 
-console.log(a + b + 8)
-console.log(typeof a, typeof b , typeof c)
+console.log(a + b + 8);
+console.log(typeof a, typeof b , typeof c);
